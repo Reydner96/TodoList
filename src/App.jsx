7b9b34482ee2dd300f882b5198062d94763efbc7
Reydner96 {Reyd} <31974666+Reydner96@users.noreply.github.com>
2023-05-30 13:13:38 -0300

    Em produção

--- src/App.jsx
+++ src/App.jsx
@@ -25,15 +25,35 @@ function App() {
     isCompleted: false,
   },
 ]);
+
+const addTodo = (text, category) => {
+  const newTodos = [
+    ...todos,
+    {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    }
+  ];
+  setTodos(newTodos);
+};
+
+const removeTodo = (id) => {
+  const newTodos = [...todos];
+  const filteredTodos = newTodos.filter((todo)=> todo.id !== id ? todo:null);
+  setTodos(filteredTodos);
+}
+
   return (
     <div className='app'>
       <h1>Lista de Tarefas</h1>
       <div className='todo-list'>
         {todos.map((todo) => (
-          <Todo todo={todo}/>
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} />
         ))}
       </div>
-      <TodoForm />
+      <TodoForm  addTodo={addTodo} />
     </div>
   )
 }
